42328d0ef49318efb615c51c9d82b87664851548
ZeRoN <romul_773@mail.ru>
2024-09-10 19:21:41 +0300

    index.js try to fetch correctly

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -1,22 +1,20 @@
 
-const sendButton = document.querySelector('#send-button');
-const inputField = document.querySelector('#input-field');
-const receiveButton = document.querySelector('#receive-button');
-const outputField = document.querySelector('#output-field');
+let sendButton = document.querySelector('.send-button');
+let inputField = document.querySelector('.input-field');
+let receiveButton = document.querySelector('#receive-button');
+let outputField = document.querySelector('#output-field');
 
 
 sendButton.addEventListener('click', () => {
-    alert("button clicked");
     fetch('http://localhost:8000/api', {
         method: "POST",
         body: textField.value
     });
-    alert("Sent!");
+    alert("Sent");
 });
 
 receiveButton.addEventListener('click', () => {
-    fetch('http://localhost:8000/api').then((response) => {
+    await fetch('http://localhost:8000/api').then((response) => {
         outputField.value = response.body;
     });
-    
 });
