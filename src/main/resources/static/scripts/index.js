01a580c546c08e86c8c7bd36cbfde032f2404593
ZeRoN <romul_773@mail.ru>
2024-09-11 01:05:28 +0300

    version 2 ready

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -1,12 +1,30 @@
 
-let sendButton = document.querySelector('.send-button');
-let textField = document.querySelector('.input-text');
+let sendButton = document.querySelector('#send-button');
+let inputField = document.querySelector('#input-text');
+let receiveButton = document.querySelector('#receive-button');
+let outputField = document.querySelector('#output-text');
 
 sendButton.addEventListener('click', () => {
     fetch('http://localhost:8000/api', {
         method: "POST",
-        body: textField.value
+        body: inputField.value
     });
     alert("Sent!");
 });
 
+
+receiveButton.addEventListener('click', async () => {
+        try {
+            let res = await fetch('http://localhost:8000/api', {
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                }
+            });
+            if (!res.ok) {
+                throw new Error('Network response was not ok ' + res.statusText);
+            }
+            outputField.value = await res.text();
+        } catch (error) {
+            console.error('Ошибка запроса:', error);
+        }
+});
